36af1e180eb08cff82f80605795acca530a5d42b
Skellzz <george.teemus@gmail.com>
2025-09-04 13:03:17 +0300

    y

---
+++ TallinnaRakenduslikKolledz/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallinnaRakenduslikKolledz.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+        public int Smartnes;
+        public string FunnyJokesMade;
+        public ICollection<PassportPhoto> PassportPhotos;
+
+
+
+
+
+
+        /* lisa kol omadust õpilasele, ise mõtled välja */
+
+    }
+}
