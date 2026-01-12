a20b4e655a26f491e47687aeca291767287ba244
Skellzz <george.teemus@gmail.com>
2026-01-12 13:51:33 +0200

    Fuck this shitti AccuWeather does not work

--- TallinnaRakenduslikKolledz/obj/Debug/net8.0/TallinnaRakenduslikKolledz.AssemblyInfo.cs
+++ TallinnaRakenduslikKolledz/obj/Debug/net8.0/TallinnaRakenduslikKolledz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TallinnaRakenduslikKolledz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+306a0469733f257a04fa29703b5731a4dd9b06bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95cb200bbeaa054cc6bbcce93ac3a94215cf3a77")]
 [assembly: System.Reflection.AssemblyProductAttribute("TallinnaRakenduslikKolledz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TallinnaRakenduslikKolledz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
